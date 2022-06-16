cce0aa9710ddd0fe5790a2923688bdec0f9e24e4
EricHdezITGuy <ehernandezm1@ucenfotec.ac.cr>
2022-06-15 20:12:24 -0600

    Proyecto terminado

---
+++ guessTheNumberPC.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import random
+
+def adivina_el_num_computadora(x):
+
+    print("===========================")
+    print(" Bienvenido al juego! ")
+    print("===========================")
+    print(f"Selecciona un número entre 1 y {x} para que la computadora intente adivinarlo...")
+
+    limite_inferior = 1
+    limite_superior = x
+
+    respuesta = ""
+    while respuesta != "c":
+        # Generar predicción
+        if limite_inferior != limite_superior:
+            prediccion = random.randint(limite_inferior, limite_superior)
+        else:
+            prediccion = limite_inferior # También podría ser el límite superior.
+
+        # Obtener respuesta del usuario
+        respuesta = input(f"Mi predicción es {prediccion}. Si es muy alta, ingresa (A). Si es muy baja, ingresa (B). Si es correcta, ingresa (C): ").lower()
+
+        if respuesta == "a":
+            limite_superior = prediccion - 1
+        elif respuesta == "b":
+            limite_inferior = prediccion + 1
+    print(f"Siii! La computadora adivinó tu número correctamente: {prediccion}")
+
+
+adivina_el_num_computadora(10)
